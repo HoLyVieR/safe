f8819eb046b231d9cd37745619b060a563f57362
Olivier Arteau <arteau.olivier@gmail.com>
2024-09-05 15:35:30 -0400

    Bind path of arguments so that they are replicated in the Docker image

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker run --rm -v $(pwd):/usr/src/safe --entrypoint=/usr/src/safe/bin/safe safe:safe-sbt "$@"
+ANALYSIS_FILE="$2"
+ANALYSIS_FOLDER="$ANALYSIS_FILE"
+
+if [ -f "$ANALYSIS_FOLDER" ]; then
+    ANALYSIS_FOLDER=$(dirname "$ANALYSIS_FOLDER")
+fi
+
+docker run --rm -v $(pwd):/usr/src/safe -v "$ANALYSIS_FOLDER:$ANALYSIS_FOLDER" --entrypoint=/usr/src/safe/bin/safe safe:safe-sbt "$@"
